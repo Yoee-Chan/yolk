d362da3f666674310dcc085b47e4e22b5d9faae8
yoee chen <yoee.chan.yy@gmail.com>
2026-03-16 00:32:13 +0800

    feat: build react+python+ts basic framework

--- python/build_exe.bat
+++ python/build_exe.bat
@@ -1,6 +1,6 @@
 @echo off
 REM 需要先安装 pyinstaller:  pip install pyinstaller
-pyinstaller --onefile hello.py
+pyinstaller --onefile agent.py
 echo.
-echo Build finished. Check the dist folder for hello.exe
+echo Build finished. Check the dist folder for agent.exe
 pause
